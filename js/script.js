a6ced158c3ad8472569e6109a576020de42d58d2
AdrianiSS <adrianzivcic@gmail.com>
2023-08-21 06:50:14 +0200

    nahratie projektu

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".toggle").on("click", function(){
+        if($(".menu").hasClass("active")){
+            $(".menu").removeClass("active");
+            $(this).find("a").html("<ion-icon name='menu_outline'></ion-icon>");
+        } else{
+            $(".menu").addClass("active");
+            $(this).find("a").html("<ion-icon name='close_outline'></ion-icon>");
+        }
+    })
+})
