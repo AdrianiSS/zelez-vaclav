159c115391dacde3b602c55b723c52c63b28ad5a
AdrianiSS <adrianzivcic@gmail.com>
2024-01-28 17:10:29 +0100

    added one site to show pictures in news + some styles

--- news.php
+++ news.php
@@ -1,11 +1,28 @@
 <?php require ('../zelez-vaclav/partials/header.php') ?>
-        <section>
+<?php require ('../zelez-vaclav/config/connection.php'); ?>
+        <section class="news">
             <h1>
-                
+                Novinky
             </h1>
-            <p>
-                
-            </p>
+            <div class="products-items">
+                    <div class="box">
+                        <?php 
+                            $i = 1;
+                            $rows = mysqli_query($conn, "SELECT * FROM upload_news ORDER BY id DESC");
+                        ?>
+                        <?php 
+                            foreach ($rows as $row) :
+                        ?>
+                        <div class="box-items">
+                            <a href="januar.php">
+                                <img class="img-click" src="../zelez-vaclav/tmp_img_news/<?php echo $row['image']; ?>" width="300" height="300" title="<?php echo $row['image']; ?>">
+                                <p class="news-text"><?php echo $row['name']; ?></p>
+                            </a>
+                            
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
         </section>
     </main>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
