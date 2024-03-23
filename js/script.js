c5e1b5dbaa0ae80e226ef4eb0e76a9c832ec7c9a
Adrian Zivcic <adrianzivcic@gmail.com>
2024-03-23 15:42:36 +0100

    bugfix changing icons

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
     $(".toggle").on("click", function(){
-        if($(".menu").hasClass("active")){
-            $(".menu").removeClass("active");
-            $(this).find("a").html("<ion-icon name='menu_outline'></ion-icon>");
-        } else{
-            $(".menu").addClass("active");
-            $(this).find("a").html("<ion-icon name='close_outline'></ion-icon>");
-        }
-    })
-})
+        $(".menu").toggleClass("active");
+    });
+});
+
