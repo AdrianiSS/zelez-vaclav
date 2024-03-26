23a56c05db80d687fcadbf535d7ccb9492e74b84
Adrian Zivcic <zivcic@tssgroup.sk>
2024-03-26 15:00:21 +0100

    added favicon and 90% responsive

--- partials/header.php
+++ partials/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="./img/favicon.png" type="image/x-icon">
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 </head>
